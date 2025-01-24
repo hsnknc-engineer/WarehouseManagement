a5a4b6db6afbcebc9e438b3e4ce67bd50d2241a8
Hasan Ekinci <ekinci.hasan@outlook.de>
2025-01-24 19:40:36 +0100

    Add Test Projekt

---
+++ WMSBackend.UnitTest/FailingTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace WMSBackend.UnitTest
+{
+    [TestFixture]
+    public class FailingTest
+    {
+        [Test]
+        public void ThisTestWillFail()
+        {
+            // Arrange
+            int expected = 5;
+            int actual = 3;
+
+
+            // Act & Assert: Absichtlich Falsch
+            Assert.That(actual, Is.EqualTo(expected), "Dieser Test schlägt fehl, weil 5 != 3.");
+        }
+    }
+}
